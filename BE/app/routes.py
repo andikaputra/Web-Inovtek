81e418af659693d57505b6e0dbb883270fa40f2e
mnurs <ipulsamudin@gmail.com>
2024-11-05 22:49:30 +0800

    Membuat API untuk get by id quizz code

--- BE/app/routes.py
+++ BE/app/routes.py
@@ -37,3 +37,12 @@ def get_quiz_kode():
         quiz_data = {'id': quiz.id, 'kode': quiz.kode, 'created_at': quiz.created_at, 'updated_at': quiz.updated_at}
         output.append(quiz_data)
     return jsonify({'data': output}), 200
+
+# READ (GET BY ID)
+@quiz_bp.route('/quiz_kode/<int:id>', methods=['GET'])
+def get_quiz_kode_by_id(id):
+    quiz = QuizKode.query.filter_by(id=id, deleted_at=None).first()
+    if not quiz:
+        return jsonify({'message': 'QuizKode not found'}), 404
+    quiz_data = {'id': quiz.id, 'kode': quiz.kode, 'created_at': quiz.created_at, 'updated_at': quiz.updated_at}
+    return jsonify({'data': quiz_data}), 200
