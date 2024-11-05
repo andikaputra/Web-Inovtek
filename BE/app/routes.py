9205e398918c54637c8938a02560392f0863fd27
mnurs <ipulsamudin@gmail.com>
2024-11-05 22:48:55 +0800

    Membuat API untuk input quizz code

--- BE/app/routes.py
+++ BE/app/routes.py
@@ -1,30 +1,29 @@
-from flask import Blueprint, request, jsonify
-# from .models import db, Quiz, Question, Answer
+from flask import Blueprint, request,send_from_directory, abort,send_file, jsonify, current_app
+from .models import db, SoalJenis, Waktu, QuizKode, Soal, SoalJawaban
+from flask_cors import CORS
+from sqlalchemy.exc import IntegrityError  # Import IntegrityError
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+import json
 
 quiz_bp = Blueprint('quiz', __name__)
+
+CORS(quiz_bp)
 
 @quiz_bp.route('/', methods=['GET'])
 def index():
     return jsonify({"message": "Welcome to the Quiz API"}), 200
 
-# @quiz_bp.route('/admin/quizzes', methods=['POST'])
-# def create_quiz():
-#     data = request.get_json()
-#     new_quiz = Quiz(kode=data['kode'])
-#     db.session.add(new_quiz)
-#     db.session.commit()
-#     return jsonify({"message": "Quiz created", "quiz_id": new_quiz.id}), 201
-
-# @quiz_bp.route('/admin/quizzes/<int:quiz_id>/questions', methods=['POST'])
-# def add_question(quiz_id):
-#     data = request.get_json()
-#     new_question = Question(quiz_id=quiz_id, text=data['text'])
-#     db.session.add(new_question)
-#     db.session.commit()
-#     return jsonify({"message": "Question added", "question_id": new_question.id}), 201
-
-# @quiz_bp.route('/quizzes/<int:quiz_id>', methods=['GET'])
-# def get_quiz(quiz_id):
-#     quiz = Quiz.query.get(quiz_id)
-#     questions = [{"id": q.id, "text": q.text} for q in quiz.questions]
-#     return jsonify({"kode": quiz.kode, "questions": questions}), 200
+# =================================QUIZ CODE==========================================================
+# CREATE
+@quiz_bp.route('/quiz_kode', methods=['POST'])
+def create_quiz_kode():
+    data = request.get_json()
+    new_quiz_kode = QuizKode(
+        kode=data['kode'],
+        created_at=datetime.utcnow()
+    )
+    db.session.add(new_quiz_kode)
+    db.session.commit()
+    return jsonify({'message': 'QuizKode created successfully', 'data': data}), 201
