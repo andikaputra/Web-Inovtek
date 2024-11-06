8b2bccf173535fce776f7cadcd41a447c48ceb77
mnurs <ipulsamudin@gmail.com>
2024-11-06 22:43:16 +0800

    Merubah tampilan dan menerapkan fungsi untuk menampilkan jawaban benar

--- FE/app/resultcorrectscreen/page.js
+++ FE/app/resultcorrectscreen/page.js
@@ -1,6 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function QuizResult() {
+  const [peserta, setPeserta] = useState([]);
+  const [kodeUnik, setKodeUnik] = useState("");
+  const [isLoading, setIsLoading] = useState(true); // State untuk loading
+  const apiUrl = process.env.apiUrl;
+
+  const mulaiUjian = async () => {
+    window.location.href = "/scoreboard";
+  };
+
+  useEffect(() => {
+    fetchNilai();
+  }, []);
+
+  const fetchNilai = async () => {
+    try {
+      const response = await axios.get(apiUrl + `/peserta_nilai/${localStorage.getItem("id_peserta")}/${localStorage.getItem("kode")}`);
+      if (response.status === 200) {
+        setPeserta(response.data.data);
+      }
+    } catch (error) {
+      console.log("Error fetching quiz_kode:", error);
+    } finally {
+      setKodeUnik(localStorage.getItem("kode_unik"));
+      setIsLoading(false); // Matikan loading setelah data diambil
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-purple-700 text-white">
+        <div className="loader mb-4"></div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-purple-700 text-white">
       <div className="absolute top-4 left-4 bg-purple-900 text-white rounded-full w-12 h-12 flex items-center justify-center font-bold text-lg">
@@ -14,7 +54,7 @@ function QuizResult() {
       </div>
 
       <div className="text-center">
-        <p className="text-3xl font-bold mb-4">Correct</p>
+        <p className="text-3xl font-bold mb-4">Benar</p>
 
         <div className="bg-green-500 w-20 h-20 rounded-full flex items-center justify-center mx-auto mb-2">
           <svg
@@ -34,7 +74,7 @@ function QuizResult() {
         </div>
 
         <div className="text-sm text-yellow-300 font-semibold mb-6">
-          Answer Streak 🔥
+          Jawaban anda benar 🔥
         </div>
 
         <div className="bg-purple-900 text-white py-2 px-6 rounded-md text-2xl font-bold mb-4">
@@ -42,18 +82,18 @@ function QuizResult() {
         </div>
 
         <p className="text-sm">
-          You’re in <span className="font-semibold">13<sup>th</sup></span> place,
+          Anda ada diperingkat <span className="font-semibold">13</span>
           <br />
-          812 points behind Roan
-        </p>
+          Poin anda sekarang {peserta.length >= 0 ? 0 : peserta.nilai} poin
+        </p> 
       </div>
 
-      <div className="absolute bottom-4 left-4 text-gray-300">
-        Den
+      <div className=" px-3 py-1 absolute bottom-4 left-4 text-gray-300 bg-gray-800 rounded-full">
+        {kodeUnik}
       </div>
 
       <div className="absolute bottom-4 right-4 bg-gray-800 text-gray-300 rounded-full px-3 py-1 text-sm font-semibold">
-        511
+        {peserta.length >= 0 ? 0 : peserta.nilai}
       </div>
     </div>
   );
