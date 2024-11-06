43ad346008437cc2361ed6f489a114d4170de290
mnurs <ipulsamudin@gmail.com>
2024-11-06 21:50:40 +0800

    Membuat config untuk link api dan base url

--- FE/next.config.mjs
+++ FE/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		apiUrl: "http://localhost:5000",
+		baseUrl:"http://localhost:3000",
+	}
+};
 
 export default nextConfig;
