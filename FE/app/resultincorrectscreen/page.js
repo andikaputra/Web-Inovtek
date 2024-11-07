5fc6870c73263c50ebe0554f12b6f091d5786a86
mnurs <ipulsamudin@gmail.com>
2024-11-07 22:25:43 +0800

    menampilkan data pada halaman jawaban salah

--- FE/app/resultincorrectscreen/page.js
+++ FE/app/resultincorrectscreen/page.js
@@ -2,34 +2,76 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { firestore } from '../firebase';
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from 'firebase/firestore';
 
 function QuizIncorrect() {
-  const [peserta, setPeserta] = useState([]);
-  const [kodeUnik, setKodeUnik] = useState("");
+  const [documentSoal, setDocumentSoal] = useState(null); 
+  const [rank, setRank] = useState(null);
+  const [points, setPoints] = useState(null);
+  const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(true); // State untuk loading
   const apiUrl = process.env.apiUrl;
 
-  const mulaiUjian = async () => {
+  const nextPage = async () => {
     window.location.href = "/scoreboard";
   };
-
-  useEffect(() => {
-    fetchNilai();
-  }, []);
-
+ 
   const fetchNilai = async () => {
     try {
-      const response = await axios.get(apiUrl + `/peserta_nilai/${localStorage.getItem("id_peserta")}/${localStorage.getItem("kode")}`);
+      const idQuizCode = localStorage.getItem("id_game");
+      const idPeserta = localStorage.getItem("id_peserta");
+      const response = await axios.get(`${apiUrl}/ranking/${idQuizCode}/${idPeserta}`);
       if (response.status === 200) {
-        setPeserta(response.data.data);
+        const data = response.data;
+        setRank(data.rank);
+        setPoints(data.points);
+        setName(data.name);
       }
     } catch (error) {
       console.log("Error fetching quiz_kode:", error);
-    } finally {
-      setKodeUnik(localStorage.getItem("kode_unik"));
+    } finally { 
       setIsLoading(false); // Matikan loading setelah data diambil
     }
   };
+
+  useEffect(() => {
+    // Ambil `kode` dari localStorage
+    const kode = localStorage.getItem("id_game");
+    
+    // Buat referensi ke dokumen spesifik menggunakan `doc` dan `kode`
+    const documentRef = doc(firestore, `soalUjian${kode}`, kode);
+    
+    // Listener untuk memantau perubahan data secara real-time pada dokumen tertentu
+    const unsubscribe = onSnapshot(documentRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const data = {
+          id: snapshot.id,
+          ...snapshot.data(),
+        };
+        setDocumentSoal(data); // Simpan data ke state
+        fetchNilai(); 
+        if(data.next_page == "scoreboard"){
+          nextPage()
+        }
+        console.log("Document data:", data); // Debug atau tampilkan data
+      } else {
+        console.log("No such document!");
+      }
+    });
+
+    // Bersihkan listener ketika komponen tidak digunakan
+    return () => unsubscribe();
+  }, []);
 
   if (isLoading) {
     return (
@@ -43,7 +85,7 @@ function QuizIncorrect() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-purple-700 text-white relative">
       <div className="absolute top-4 left-4 bg-purple-900 text-white rounded-full w-12 h-12 flex items-center justify-center font-bold text-lg">
-        11
+        {documentSoal.nomor_soal}
       </div>
 
       <div className="absolute top-4 flex justify-center items-center">
@@ -77,16 +119,16 @@ function QuizIncorrect() {
         </div>
 
         <p className="text-sm">
-          Anda ada diperingkat <span className="font-semibold">13</span>
+          Anda ada diperingkat <span className="font-semibold">{rank}</span>
         </p>
       </div>
 
       <div className=" px-3 py-1 absolute bottom-4 left-4 text-gray-300 bg-gray-800 rounded-full">
-        {kodeUnik}
+        {name}
       </div>
 
       <div className="absolute bottom-4 right-4 bg-gray-800 text-gray-300 rounded-full px-3 py-1 text-sm font-semibold">
-        {peserta.length >= 0 ? 0 : peserta.nilai}
+        {points <= 0 ? 0 : points}
       </div>
     </div>
   );
