ee6d6259cbd41fa5218cfd4531b601f2de3ec0e9
mnurs <ipulsamudin@gmail.com>
2024-11-07 22:30:43 +0800

    membuat tampilan dan memampilkan halaman jawaban peserta di halaman admin

---
+++ FE/app/admin/quizzsoal/page.js
@@ -0,0 +1,480 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios"; // Pastikan menginstall axios atau gunakan fetch
+import { useLocation } from "react-router-dom";
+
+
+function QuizAdmin() {
+  const [kode, setKode] = useState("");
+  const [question, setQuestion] = useState("");
+  const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
+  const [type, setType] = useState("");
+  const [timeLimit, setTimeLimit] = useState("");
+  const [answersQuiz, setAnswersQuiz] = useState([
+    { text: "", color: "bg-red-500", correct: false, image: null, file:null },
+    { text: "", color: "bg-blue-500", correct: false, image: null, file:null },
+    { text: "", color: "bg-yellow-500", correct: false, image: null, file:null },
+    { text: "", color: "bg-green-500", correct: true, image: null, file:null },
+  ]);
+  const [answersTF, setAnswersTF] = useState([
+    { text: "True", color: "bg-red-500", correct: false},
+    { text: "False", color: "bg-blue-500", correct: true}
+  ]);
+  const [answersUraian, setAnswersUraian] = useState([
+    { text: "", color: "bg-red-500", correct: false}
+  ]);
+  const [quizList, setQuizList] = useState([]); // Menyimpan list soal yang telah diinput
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // State untuk menyimpan opsi dari API
+  const [questionTypes, setQuestionTypes] = useState([]);
+  const [timeLimits, setTimeLimits] = useState([]);
+
+
+  const [formData, setFormData] = useState({
+    id_quiz_kode: '',
+    id_soal_jenis: '',
+    id_waktu: '',
+    pertanyaan: '',
+    layout: ''
+  });
+  const [message, setMessage] = useState('');
+  const apiUrl = process.env.apiUrl;
+
+ const fetchDataType = async () => {
+    try { 
+      const response = await axios.get(apiUrl + "/soal_jenis"); // Ganti URL dengan endpoint API sebenarnya
+      console.log(response.data.data)
+      setQuestionTypes(response.data.data); // Pastikan API mengembalikan data dalam format { types: [...] } 
+    } catch (error) {
+      console.error("Error fetching quiz options:", error);
+    }
+  };
+
+ const fetchDataTIme = async () => {
+    try {
+      const response = await axios.get(apiUrl + "/waktu"); // Ganti URL dengan endpoint API sebenarnya
+      console.log(response.data.data)
+      setTimeLimits(response.data.data); // Pastikan API mengembalikan data dalam format { types: [...] } 
+    } catch (error) {
+      console.error("Error fetching quiz options:", error);
+    }
+  };
+
+ const fetchQuestions = async (kode) => {
+    try {
+      setQuizList([]);
+      const response = await axios.get(apiUrl + `/soal/${kode}`);
+      setQuizList(response.data); // Simpan data soal di state
+      setLoading(false);
+    } catch (err) {
+      setError('Failed to fetch questions');
+      setLoading(false);
+    }
+  };
+
+
+  const handleSubmit = async () => { 
+    
+    // Buat FormData dan tambahkan file serta data lainnya
+    const data = new FormData();
+    data.append('file', file);
+    data.append('id_quiz_kode', kode);
+    data.append('id_soal_jenis', type);
+    data.append('id_waktu', timeLimit);
+    data.append('pertanyaan', question);
+    data.append('layout', 1);
+
+    // Tambahkan array of objects
+    answersQuiz.forEach((answer, index) => {
+      // Konversi setiap objek menjadi string JSON 
+      data.append(`answers[${index}]`, JSON.stringify({
+        text: answer.text,
+        correct: answer.correct
+      }));
+
+      if (answer.file) {
+        data.append(`file_${index}`, answer.file);
+      }
+    });
+
+    try {
+      const response = await axios.post(apiUrl+ '/soal', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if (response.status === 200) { // Cek jika status respons adalah 200
+        setMessage('File uploaded successfully!');
+        setQuestion("");
+        setImage(null);
+        setFile(null);
+        setType("");
+        setTimeLimit("");
+        setAnswersQuiz([
+          { text: "", color: "bg-red-500", correct: false, image: null, file:null },
+          { text: "", color: "bg-blue-500", correct: false, image: null, file:null },
+          { text: "", color: "bg-yellow-500", correct: false, image: null, file:null },
+          { text: "", color: "bg-green-500", correct: false, image: null, file:null },
+        ]);
+        fetchQuestions(kode);
+      } else {
+        setMessage('Failed to upload file.');
+      }
+    } catch (error) {
+      setMessage('Failed to upload file.');
+    }
+  };
+
+ // Fungsi untuk menghapus soal berdasarkan ID
+  const handleDelete = async (id) => {
+    // Menampilkan dialog konfirmasi
+    const confirmed = window.confirm("Apakah Anda yakin ingin menghapus soal ini?");
+    
+    // Jika pengguna menekan "OK", lanjutkan penghapusan
+    if (confirmed) {
+      try {
+        await axios.delete(`${apiUrl}/soal/${id}`);
+        setQuizList(quizList.filter((question) => question.id !== id)); // Hapus soal dari state
+      } catch (error) {
+        console.error('Failed to delete question', error);
+      }
+    }
+  };
+
+    // Fetch data dari API
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const kodeParam = queryParams.get("kode");
+    setKode(kodeParam);
+    fetchDataType();
+    fetchDataTIme();
+    fetchQuestions(kodeParam);
+  }, []);
+
+  const handleImageChange = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]));
+    setFile(e.target.files[0]);
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null); // Menghapus gambar soal
+    setFile(null);
+  };
+
+  const handleAnswerImageChange = (index, e) => {
+    const newAnswers = [...answersQuiz];
+    newAnswers[index].image = URL.createObjectURL(e.target.files[0]);
+    newAnswers[index].file =  e.target.files[0];
+    newAnswers[index].text = ""; // Mengosongkan teks jika gambar diunggah
+    setAnswersQuiz(newAnswers);
+  };
+
+  const handleRemoveAnswerImage = (index) => {
+    const newAnswers = [...answersQuiz];
+    newAnswers[index].image = null; // Menghapus gambar pada jawaban tertentu
+    setAnswersQuiz(newAnswers);
+  };
+
+  const handleAnswerChange = (index, text) => {
+    const newAnswers = [...answersQuiz];
+    newAnswers[index].text = text;
+    newAnswers[index].image = null; // Mengosongkan gambar jika teks diinput
+    setAnswersQuiz(newAnswers);
+  };
+
+  const handleCorrectAnswer = (index) => { 
+    console.log(answersQuiz);
+    if(type == 1) {
+       const newAnswers = answersQuiz.map((answer, i) => (
+        i === index ? { ...answer, correct: !answer.correct } : answer
+    ));
+    
+    setAnswersQuiz(newAnswers); 
+    }else if(type == 2){
+      const newAnswers = answersTF.map((answer, i) => ({
+        ...answer,
+        correct: i === index,
+      }));
+      setAnswersTF(newAnswers); 
+    }
+  };
+
+  const handleSaveQuestion = () => {
+    // setQuizList([
+    //   ...quizList,
+    //   {
+    //     question,
+    //     image,
+    //     type,
+    //     timeLimit,
+    //     answers,
+    //   },
+    // ]);
+    handleSubmit();
+    
+  };
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center p-6"
+      style={{
+        backgroundImage: `url('/src/img/bg.jpg')`,
+      }}
+    >
+      <div className="max-w-3xl w-full bg-white bg-opacity-80 shadow-lg rounded-lg p-6 mb-8">
+        <h2 className="text-2xl font-bold text-center mb-6">Input Soal Baru</h2>
+
+        {/* Input Pertanyaan */}
+        <div className="mb-4"> 
+          <label className="block font-semibold mb-2">Pertanyaan</label>
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+            placeholder="Masukkan pertanyaan di sini"
+          />
+        </div>
+
+        {/* Input Tipe Pertanyaan */}
+        <div className="mb-4">
+          <label className="block font-semibold mb-2">Tipe Pertanyaan</label>
+          <select
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+          >
+            <option value=""> {/* Pastikan 'value' sesuai dengan key dari data API */}
+                  Pilih Tipe Pertanyaan {/* Pastikan 'label' sesuai dengan key dari data API */}
+            </option>
+            {questionTypes.length > 0 ? (
+              questionTypes.map((item, index) => (
+                <option key={index} value={item.id}> {/* Pastikan 'value' sesuai dengan key dari data API */}
+                  {item.nama} {/* Pastikan 'label' sesuai dengan key dari data API */}
+                </option>
+              ))
+            ) : (
+              <option>Loading...</option> // Ditampilkan saat data belum dimuat
+            )}
+          </select>
+        </div>
+
+
+        {/* Batas Waktu */}
+        <div className="mb-4">
+          <label className="block font-semibold mb-2">Batas Waktu (detik)</label>
+          <select
+            value={timeLimit}
+            onChange={(e) => setTimeLimit(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded"
+          >
+            <option value=""> {/* Pastikan 'value' sesuai dengan key dari data API */}
+                  Pilih Waktu {/* Pastikan 'label' sesuai dengan key dari data API */}
+            </option>
+            {timeLimits.length > 0 ? (
+              timeLimits.map((item, index) => (
+                <option key={index} value={item.id}> {/* Pastikan 'value' sesuai dengan key dari data API */}
+                  {item.nama} {/* Pastikan 'label' sesuai dengan key dari data API */}
+                </option>
+              ))
+            ) : (
+              <option>Loading...</option> // Ditampilkan saat data belum dimuat
+            )}
+          </select>
+        </div> 
+
+        {/* Input Gambar */}
+        <div className="mb-4">
+          <label className="block font-semibold mb-2">Gambar Soal</label>
+          <input type="file" onChange={handleImageChange} className="w-full" />
+          {image && (
+            <div className="mt-4">
+              <img src={image} alt="Preview" className="w-full h-64 object-cover rounded" />
+              <button onClick={handleRemoveImage} className="mt-2 px-4 py-2 bg-red-600 text-white rounded">
+                Hapus Gambar
+              </button>
+            </div>
+          )}
+        </div>
+
+        {
+          type == 1 ? (
+            <div className="mb-6">
+              <label className="block font-semibold mb-2">Pilihan Jawaban</label>
+              {answersQuiz.map((answer, index) => (
+                <div key={index} className="flex items-center mb-2">
+                  {!answer.image && (
+                    <input
+                      type="text"
+                      value={answer.text}
+                      onChange={(e) => handleAnswerChange(index, e.target.value)}
+                      className={`w-full p-2 border border-gray-300 rounded ${answer.color} text-white font-semibold mr-4`}
+                      placeholder={`Jawaban ${index + 1}`}
+                    />
+                  )}
+
+                  {!answer.text && (
+                    <input type="file" onChange={(e) => handleAnswerImageChange(index, e)} className="mr-4" />
+                  )}
+
+                  {answer.image && (
+                    <div className="flex items-center">
+                      <img
+                        src={answer.image}
+                        alt="Answer Preview"
+                        className="w-12 h-12 object-cover rounded mr-2"
+                      />
+                      <button
+                        onClick={() => handleRemoveAnswerImage(index)}
+                        className="px-2 py-1 bg-red-600 text-white rounded"
+                      >
+                        Hapus
+                      </button>
+                    </div>
+                  )}
+                  <button
+                    onClick={() => handleCorrectAnswer(index)}
+                    className={`px-4 py-2 rounded ${
+                      answer.correct ? "bg-green-600 text-white" : "bg-gray-300 text-gray-800"
+                    }`}
+                  >
+                    {answer.correct ? "Benar" : "Pilih"}
+                  </button>
+                </div>
+              ))}
+            </div>
+          ) : type == 2 ? (
+            <div className="mb-6">
+              <label className="block font-semibold mb-2">Pilihan Jawaban</label>
+              {answersTF.map((answer, index) => (
+                <div key={index} className="flex items-center mb-2">
+                  
+                  <button 
+                    className={`w-full p-2 border border-gray-300 rounded ${answer.color} text-white font-semibold mr-4
+                    }`}
+                  >
+                    {answer.text}
+                  </button>
+
+                  <button
+                    onClick={() => handleCorrectAnswer(index)}
+                    className={`px-4 py-2 rounded ${
+                      answer.correct ? "bg-green-600 text-white" : "bg-gray-300 text-gray-800"
+                    }`}
+                  >
+                    {answer.correct ? "Benar" : "Pilih"}
+                  </button>
+                </div>
+              ))}
+            </div>
+          ) : type == 3 ? (
+            <div className="mb-6">
+              <label className="block font-semibold mb-2">Jawaban</label>
+              {answersUraian.map((answer, index) => (
+                <div key={index} className="flex items-center mb-2">
+                  {!answer.image && (
+                    <input
+                      type="text"
+                      value={answer.text}
+                      onChange={(e) => handleAnswerChange(index, e.target.value)}
+                      className={`w-full p-2 border border-gray-300 rounded  text-white font-semibold mr-4`}
+                      placeholder={`Jawaban`}
+                    />
+                  )} 
+                </div>
+              ))}
+            </div>
+          ) : null
+        }
+
+        
+
+        {/* Tombol Simpan */}
+        <div className="text-center">
+          <button
+            onClick={handleSaveQuestion}
+            className="bg-blue-600 text-white px-6 py-2 rounded font-semibold hover:bg-blue-700"
+          >
+            Simpan Soal
+          </button>
+        </div>
+      </div>
+
+     <div className="max-w-3xl w-full">
+        <h3 className="text-2xl font-bold mb-4 text-white">Daftar Soal</h3>
+        {quizList.map((question, index) => ( 
+          <div
+            key={question.id}
+            style={{
+              backgroundColor: '#fff',
+              padding: '20px',
+              marginBottom: '20px',
+              borderRadius: '8px',
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <div>
+              <h2>{index + 1}. {question.pertanyaan}</h2>
+              {question.file && (
+                <p>
+                  <a href={apiUrl + `/view/${question.file.split('\\').pop()}`} target="_blank" rel="noopener noreferrer">
+                    View File  
+                  </a>
+                </p>
+              )}
+              <h3>Answers:</h3>
+              <ul>
+                {question.jawaban.map((answer, i) => {
+                  const alphabet = String.fromCharCode(65 + i);
+                  
+                  // Cek apakah `text_jawaban` adalah path file atau teks biasa
+                  const isFile = answer.text_jawaban && (answer.text_jawaban.includes('/') || answer.text_jawaban.match(/\.(pdf|jpg|jpeg|png|docx|txt)$/i));
+                  const fileName = isFile ? answer.text_jawaban.split('\\').pop() : answer.text_jawaban;
+
+                  return (
+                    <li key={answer.id} style={{ color: answer.correct ? 'green' : 'black' }}>
+                      {alphabet}.{" "}
+                      {isFile ? (
+                        <a href={apiUrl + `/view/${fileName}`} target="_blank" rel="noopener noreferrer">
+                          View Answer File
+                        </a>
+                      ) : (
+                        answer.text_jawaban
+                      )}
+                      {answer.correct && ' (Correct)'}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            
+            <button
+              onClick={() => handleDelete(question.id)}
+              style={{
+                marginLeft: '20px',
+                padding: '8px 16px',
+                color: 'white',
+                backgroundColor: 'red',
+                border: 'none',
+                borderRadius: '4px',
+                alignSelf: 'flex-start'
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default QuizAdmin;
