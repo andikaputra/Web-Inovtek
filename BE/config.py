e950d1f66ac7c31cf4c4e709a3df95076ea1bff4
mnurs <ipulsamudin@gmail.com>
2024-11-27 17:22:10 +0800

    menampilkan config untuk database

---
+++ BE/config.py
@@ -0,0 +1,13 @@
+import os  # Make sure to import the os module
+
+class Config:
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'quizz')
+
+    # Set SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional: disable track modifications to save resources
